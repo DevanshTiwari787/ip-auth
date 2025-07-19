6c95b427e301f65e284054514b09ec5e37bd81f8
DevanshTiwari787 <devanshtiwari2610@gmail.com>
2025-07-19 20:47:39 +0530

    commit for chetan 3

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -57,4 +57,33 @@ router.post("/login", async (req, res) => {
     }
 
 })
+
+async function auth(req, res,next){
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ error: "No token provided" });
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+        if (decoded.id !== req.params.id) {
+            return res.status(403).json({ error: "Forbidden: You can only access your own profile" });
+        }
+        req.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: "Invalid or expired token" });
+    }
+}
+router.get("/get-profile/:id", auth, async (req, res) => {
+    try {
+        res.json({
+            message: "User fetched successfully",
+            user: req.user
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 module.exports = router;
